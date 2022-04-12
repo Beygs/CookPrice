af15f302d3047699589a6af5907e636f92eb39e1
Beygs <boris.gilles@hotmail.fr>
2022-04-12 18:49:08 +0200

    profile picture proxy

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image";
 
 export default function Component() {
   const { data: session } = useSession()
@@ -8,6 +9,13 @@ export default function Component() {
   if (session) {
     return (
       <>
+        <Image
+          src={`/api/imageproxy?url=${encodeURIComponent(session.user.image)}`}
+          width="100px"
+          height="100px"
+          alt="Profile picture"
+        /><br />
+        Hello {session.user.name}! <br />
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
